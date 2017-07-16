d0408f6dc420364317a994cba1158a81efde91e6
Coen Mooij <coen.mooij@werkspot.nl>
2017-07-16 15:27:08 +0200

    Add wonderful quotes to overview

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ new Vue({
                 description: 'A turn-based game to beat a monster with 4 simple moves. It will attack you as well.',
                 demo: '/monster-game',
                 sourceCode: 'https://github.com/coenmooij/vuejs-exercises/tree/master/monster-game',
+            }, {
+                name: 'Wonderful quotes',
+                description: 'A quotes displaying app in which you can add and remove quotes',
+                demo: '/wonderful-quotes',
+                sourceCode: 'https://github.com/coenmooij/vuejs-exercises/tree/master/wonderful-quotes',
             },
         ],
     }
