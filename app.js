eb56ecaaa7c19265934ce36870bde6a4ea8d0c50
Coen Mooij <coenmooij@gmail.com>
2025-08-05 13:00:57 +0200

    Update links

--- app.js
+++ app.js
@@ -5,22 +5,22 @@ new Vue({
             {
                 name: 'Monster Game',
                 description: 'A turn-based game to beat a monster with 4 simple moves.',
-                demo: '/monster-game',
+                demo: '/vuejs-exercises/monster-game',
                 sourceCode: 'https://github.com/coenmooij/vuejs-exercises/tree/master/monster-game',
             }, {
                 name: 'Wonderful quotes',
                 description: 'A quotes displaying app in which you can add and remove quotes',
-                demo: '/wonderful-quotes',
+                demo: '/vuejs-exercises/wonderful-quotes',
                 sourceCode: 'https://github.com/coenmooij/vuejs-exercises/tree/master/wonderful-quotes',
             }, {
                 name: 'Super Quiz',
                 description: 'A simple math quiz with some cool css animation',
-                demo: '/super-quiz',
+                demo: '/vuejs-exercises/super-quiz',
                 sourceCode: 'https://github.com/coenmooij/vuejs-exercises/tree/master/super-quiz',
             }, {
                 name: 'Stock Trader',
                 description: 'A stock trading game which you can buy and sell stocks',
-                demo: '/stock-trader',
+                demo: '/vuejs-exercises/stock-trader',
                 sourceCode: 'https://github.com/coenmooij/vuejs-exercises/tree/master/stock-trader',
             },
         ],
